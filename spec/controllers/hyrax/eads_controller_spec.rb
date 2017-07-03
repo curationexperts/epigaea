5dc4de00eb976eaa1678752f30d81ba6b6755dc1
Jamie Little <jamie@jamielittle.org>
2017-07-03 14:51:49 -0400

    Add EAD work type

---
+++ spec/controllers/hyrax/eads_controller_spec.rb
@@ -0,0 +1,9 @@
+# Generated via
+#  `rails generate hyrax:work Ead`
+require 'rails_helper'
+
+RSpec.describe Hyrax::EadsController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
