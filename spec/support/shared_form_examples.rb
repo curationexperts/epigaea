d150f7f5014e0b564657a99e6363f4faadb81ed9
Jamie Little <jamie@jamielittle.org>
2017-07-13 17:29:17 -0400

    Add specs for the admin attributes

--- spec/support/shared_form_examples.rb
+++ spec/support/shared_form_examples.rb
@@ -4,13 +4,10 @@ shared_examples 'a form with Tufts metadata attributes' do
       expect(form.required_fields).to eq([:title, :displays_in])
     end
     it 'has Tufts terms' do
-      expect(form.terms).to include(:title, :displays_in, :geographic_name, :held_by, :alternative_title,
+      expect(form.terms).to include(:displays_in, :geographic_name, :held_by, :alternative_title,
                                     :abstract, :table_of_contents, :primary_date, :date_accepted,
-                                    :date_available, :date_copyrighted, :date_issued)
-    end
-    it 'has Tufts admin terms' do
-      expect(form.terms).to include(:steward, :created_by, :internal_note, :audience, :end_date,
-                                    :accrual_policy, :license)
+                                    :date_available, :date_copyrighted, :date_issued, :steward, :created_by,
+                                    :internal_note, :audience, :embargo_note, :end_date, :accrual_policy, :license, :rights_note)
     end
   end
 end
