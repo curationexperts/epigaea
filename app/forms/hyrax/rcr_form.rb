a3e5607da7059f2b392a332fba1a20f99b2aa1ff
Jamie Little <jamie@jamielittle.org>
2017-07-14 10:14:29 -0400

    Add a custom metadata service and use it in forms to prevent user from making multiple titles

--- app/forms/hyrax/rcr_form.rb
+++ app/forms/hyrax/rcr_form.rb
@@ -5,5 +5,16 @@ module Hyrax
     self.model_class = ::Rcr
     self.terms += Tufts::Terms.shared_terms
     self.required_fields = [:title, :displays_in]
+    self.field_metadata_service = Tufts::MetadataService
+
+    def self.model_attributes(_)
+      attrs = super
+      attrs[:title] = Array(attrs[:title]) if attrs[:title]
+      attrs
+    end
+
+    def title
+      super.first || ""
+    end
   end
 end
