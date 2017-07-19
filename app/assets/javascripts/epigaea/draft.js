70a4e08a8c13109879ae1a34cf75882a57eaba1d
Jamie Little <jamie@jamielittle.org>
2017-07-19 08:00:05 -0400

    Basic save button

--- app/assets/javascripts/epigaea/draft.js
+++ app/assets/javascripts/epigaea/draft.js
@@ -1,37 +1,60 @@
 var Draft = {
-  saveDraft: function () {
-    Draft.deleteDraft()
+  init: () => {
+    Draft.isSaved()
+    Draft.bindSaveDraftClick()
+    Draft.deleteDraftOnSave()
+  },
+  save: () => {
+    Draft.delete()
     var request = new XMLHttpRequest()
     var form = document.querySelector('.simple_form')
     var formData = new FormData(form)
     formData.delete('_method')
 
-    request.open('POST', `/draft/save_draft/${this.getWorkId()}`)
+    request.open('POST', `/draft/save_draft/${Draft.getWorkId()}`)
     request.send(formData)
 
-    request.onreadystatechange = function () {
-      if (request.readyState == XMLHttpRequest.DONE) {
+    request.onreadystatechange = () => {
+      if (request.readyState === XMLHttpRequest.DONE) {
         console.log(request.responseText)
+        Draft.isSaved()
       }
     }
   },
-  applyDraft: function () {
-    $.get('/draft/apply_draft/' + this.getWorkId(), (data) => {
+  apply: () => {
+    $.get('/draft/apply_draft/' + Draft.getWorkId(), (data) => {
       var formData = JSON.parse(data.draft)
-      console.log(formData)
+      for (key in formData) {
+        if (formData.hasOwnProperty(key)) {
+          $(`[id$=${key}]`).val(formData[key])
+	  }
+      }
     })
   },
-  deleteDraft: function () {
-    $.post('/draft/delete_draft/' + this.getWorkId(), (data) => {
+  delete: () => {
+    $.post('/draft/delete_draft/' + Draft.getWorkId(), (data) => {
       console.log(data)
     })
   },
-  draftSaved: function () {
-    $.get('/draft/draft_saved/' + this.getWorkId(), (data) => {
-      console.log(data)
+  isSaved: () => {
+    $.get('/draft/draft_saved/' + Draft.getWorkId(), (data) => {
+      if (data.status) {
+        $('.draft-bar').append(`<span class='draft-status'>This work has a saved draft.</span>`)
+        Draft.apply()
+      }
     })
   },
-  getWorkId: function () {
+  getWorkId: () => {
     return document.querySelector('.simple_form').id.split('_')[2]
+  },
+  bindSaveDraftClick: () => {
+    $('.save-draft').on('click', () => {
+      Draft.save()
+    })
+  },
+  deleteDraftOnSave: () => {
+    $('[name=save_with_files]').on('click', () => {
+      Draft.delete()
+    })
   }
 }
