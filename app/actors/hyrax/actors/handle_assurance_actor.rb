dadec9b0b709e2ad45e18d56f5f814f26abe12b6
Tom Johnson <tom@dp.la>
2017-07-25 13:39:44 -0700

    Fix HandleAssuranceActor to be a well behaved Hyrax Actor
    A Hyrax Actor should manually invoke its `next_actor` unless it intends to
    terminate and pop out of the actor stack. This better behaved version of the
    `HandleAssuranceActor` does this, and also declines to enqueue handle creation
    if something lower in the stack terminates `false`. This means that if
    `save` (called by `Hyrax::Actors::BaseActor` near the bottom of the stack)
    fails, we won't bother to enqueue a Handle job.
    
    Tests are added for the Actor in an `ActionDispatch::MiddlewareStack` context.

--- app/actors/hyrax/actors/handle_assurance_actor.rb
+++ app/actors/hyrax/actors/handle_assurance_actor.rb
@@ -3,35 +3,57 @@ module Hyrax
     ##
     # An actor that ensures a handle is registered and pointing at the correct
     # object.
+    #
+    # Enqueues a job to mint or update a handle. The `#create` and `#update`
+    # methods punt their work to `#ensure_handle`. If an actor lower in the
+    # middleware stack returns `false` (an error state), nothing will be
+    # equeued.
+    #
+    # @example use in middleware
+    #   stack = ActionDispatch::MiddlewareStack.new.tap do |middleware|
+    #     # middleware.use OtherMiddleware
+    #     middleware.use HandleAssuranceActor
+    #     # middleware.use MoreMiddleware
+    #   end
+    #
+    #   env = Hyrax::Actors::Environment.new(object, ability, attributes)
+    #   last_actor = Hyrax::Actors::Terminator.new
+    #   stack.build(last_actor).create(env)
+    #
     class HandleAssuranceActor < AbstractActor
       ##
       # @param env [Hyrax::Actors::Enviornment]
       #
-      # @return [Boolean] true
+      # @return [Boolean]
       def create(env)
-        ensure_handle(object: env.curation_concern)
-        true
+        next_actor.create(env) && ensure_handle(object: env.curation_concern)
       end
 
       ##
       # @param env [Hyrax::Actors::Enviornment]
       #
-      # @return [Boolean] true
+      # @return [Boolean]
       def update(env)
-        ensure_handle(object: env.curation_concern)
-        true
+        next_actor.update(env) && ensure_handle(object: env.curation_concern)
       end
 
       ##
+      # Enqueue handle creation or update.
+      #
+      # If `object` has no exiting `identifier` a 'HandleRegisterJob` is
+      # enqueued. If an identifier exists, a `HandleUpdateJob` is enqueued
+      # instead.
+      #
       # @param object [ActiveFedora::Base]
       #
-      # @return [void]
+      # @return [Boolean] true
       def ensure_handle(object:)
         if object.identifier.empty?
           HandleRegisterJob.perform_later(object)
         else
           HandleUpdateJob.perform_later(object)
         end
+        true
       end
     end
   end
