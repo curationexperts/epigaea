9aa5644be4b534456adfe99ee18323623344d850
Tom Johnson <tom@dp.la>
2017-08-03 16:34:12 -0700

    Fix RSpec deprecations
    Some code pulled in from old MIRA had some `should` syntax that needed to be
    cleaned up. Various other issues in the specs and style for the relevant files
    are cleaned up.

--- spec/controllers/contribute_controller_spec.rb
+++ spec/controllers/contribute_controller_spec.rb
@@ -2,90 +2,119 @@ require 'rails_helper'
 
 describe ContributeController do
   let(:deposit_type) { FactoryGirl.create(:deposit_type) }
-  describe "for a not-signed in user" do
-    describe "new" do
-      it "redirects to sign in", :no_ci do
+
+  describe 'for a not-signed in user' do
+    describe 'GET #new' do
+      it 'redirects to sign in' do
         pending('waiting for role solution')
         get :new
-        response.should redirect_to new_user_session_path(locale: :en)
+
+        expect(response).to redirect_to new_user_session_path(locale: :en)
       end
     end
-    describe "create" do
-      it "redirects to sign in", :no_ci do
-        post :create
+
+    describe 'POST #create' do
+      it 'redirects to sign in' do
         pending('waiting for role solution')
-        response.should redirect_to new_user_session_path(locale: :en)
+        post :create
+
+        expect(response).to redirect_to new_user_session_path(locale: :en)
       end
     end
   end
 
-  describe "for a signed in user" do
+  describe 'for a signed in user' do
     let(:user) { FactoryGirl.create(:user) }
+
     before { sign_in user }
 
-    describe "GET '/'" do
-      it "returns http success", :no_ci do
+    describe 'GET #index' do
+      it 'returns http success' do
         get :index
-        response.should be_success
+
+        expect(response).to be_success
       end
     end
 
-    describe "GET 'license'" do
-      it "returns http success", :no_ci do
+    describe 'GET #license' do
+      it 'returns http success' do
         get 'license'
-        response.should be_success
+
+        expect(response).to be_success
       end
     end
 
-    describe "GET 'new'" do
-      it "redirects to contribute home when no deposit type is specified", :no_ci do
+    describe 'GET #new' do
+      it 'redirects to contribute home when no deposit type is specified' do
         get 'new'
-        response.should redirect_to contributions_path
+
+        expect(response).to redirect_to contributions_path
       end
 
       describe 'with valid deposit_type' do
-        let(:deposit_type) { FactoryGirl.create(:deposit_type, display_name: 'Test Option', deposit_view: 'generic_deposit') }
+        let(:deposit_type) do
+          FactoryGirl.create(:deposit_type,
+                             display_name: 'Test Option',
+                             deposit_view: 'generic_deposit')
+        end
 
         render_views
 
-        it 'renders the correct template', :no_ci do
+        it 'renders the correct template' do
           get 'new', params: { deposit_type: deposit_type.id }
-          response.should render_template('contribute/deposit_view/_generic_deposit')
+
+          expect(response)
+            .to render_template('contribute/deposit_view/_generic_deposit')
         end
 
-        it 'includes deposit license text', :no_ci do
+        it 'includes deposit license text' do
           get 'new', params: { deposit_type: deposit_type.id }
-          response.body.should have_content deposit_type.deposit_agreement
+
+          expect(response.body)
+            .to have_content deposit_type.deposit_agreement
         end
 
-        it 'sets deposit_type and contribution', :no_ci do
+        it 'sets deposit_type and contribution' do
           get :new, params: { deposit_type: deposit_type }
-          assigns(:deposit_type).should eq(deposit_type)
-          assigns(:contribution).should_not be_nil
+
+          expect(assigns(:deposit_type)).to eq(deposit_type)
+          expect(assigns(:contribution)).not_to be_nil
         end
       end
     end
 
-    describe "GET 'redirect'" do
-      it "redirects to contribute", :no_ci do
+    describe 'GET #redirect' do
+      it 'redirects to contribute' do
         pending('waiting for role solution')
         get 'redirect'
+
         response.should redirect_to contributions_path
       end
     end
 
-    describe "POST 'create'" do
-      it "redirects when no deposit type is specified", :no_ci do
-        post :create, params: { contribution: { title: 'Sample', description: 'Description', creator: user.display_name } }
-        response.should redirect_to contributions_path
+    describe 'POST #create' do
+      let(:params) do
+        { contribution: { title:       'Sample',
+                          description: 'Description',
+                          creator:     user.display_name } }
+      end
+
+      it 'redirects when no deposit type is specified' do
+        post :create, params: params
+
+        expect(response).to redirect_to contributions_path
       end
 
       describe 'with valid deposit_type' do
         before { Pdf.destroy_all }
 
-        let(:uploaded_file) { fixture_file_upload('/local_object_store/data01/tufts/central/dca/MISS/archival_pdf/MISS.ISS.IPPI.archival.pdf', 'application/pdf') }
+        let(:uploaded_file) do
+          path = '/local_object_store/data01/tufts/central/dca/MISS/' \
+                 'archival_pdf/MISS.ISS.IPPI.archival.pdf'
+          fixture_file_upload(path, 'application/pdf')
+        end
 
-        it 'succeeds and stores file attachments', :no_ci do
+        it 'succeeds and stores file attachments' do
           pending 'Waiting for roles solution'
           post :create, params: { contribution: { title: 'Sample', description: 'Description goes here',
                                                   creator: 'Someone', attachment: uploaded_file }, deposit_type: deposit_type }
@@ -93,7 +122,7 @@ describe ContributeController do
           expect(Pdf.all.length).to eq(1)
         end
 
-        it 'automaticallies populate static fields', :no_ci do
+        it 'automaticallies populate static fields' do
           pending 'Waiting for roles solution'
           post :create, params: { contribution: { title: 'Sample', description: 'User supplied brief description',
                                                   creator: 'John Doe', attachment: uploaded_file }, deposit_type: deposit_type }
@@ -106,12 +135,19 @@ describe ContributeController do
           expect(contribution.format).to eq ['application/pdf']
         end
 
-        it "lists deposit_method as self deposit", :no_ci do
+        it 'lists deposit_method as self deposit' do
           pending 'Waiting for roles solution'
           now = Time.zone.now
           Time.stub(:now).and_return(now)
-          post :create, params: { contribution: { title: 'Sample', description: 'Description of goes here',
-                                                  creator: 'Mickey Mouse', attachment: uploaded_file }, deposit_type: deposit_type }
+
+          post :create, params: {
+            contribution: { title:       'Sample',
+                            description: 'Description of goes here',
+                            creator:     'Mickey Mouse',
+                            attachment:  uploaded_file },
+            deposit_type: deposit_type
+          }
+
           contribution = Pdf.find(assigns[:contribution].id)
           expect(contribution.note.first).to eq "Mickey Mouse self-deposited on #{now.strftime('%Y-%m-%d at %H:%M:%S %Z')} using the Deposit Form for the Tufts Digital Library"
           expect(contribution.date_available).to eq [now.to_s]
@@ -119,9 +155,15 @@ describe ContributeController do
           expect(contribution.createdby).to eq Contribution::SELFDEP
         end
 
-        it 'requires a file attachments', :no_ci do
-          post :create, params: { contribution: { title: 'Sample', description: 'Description of uploaded file goes here', creator: user.display_name }, deposit_type: deposit_type }
-          response.should render_template('new')
+        it 'requires a file attachments' do
+          params = { contribution: { title:       'Sample',
+                                     description: 'Description of uploaded file goes here',
+                                     creator:      user.display_name },
+                     deposit_type: deposit_type }
+
+          post :create, params: params
+
+          expect(response).to render_template('new')
         end
       end
     end
