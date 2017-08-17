aa82d1bc5d821f5ceb6f525d8898753efa58a3b8
Jamie Little <jamie@jamielittle.org>
2017-08-17 17:19:25 -0400

    Refactoring the draft js to be more modular

--- app/assets/javascripts/tufts.js
+++ app/assets/javascripts/tufts.js
@@ -2,5 +2,10 @@ var Tufts = {
   qrStatus: function () {
     var QrStatus = require('tufts/qr_status')
     return new QrStatus()
+  },
+  draft: function() {
+    var Draft = require('tufts/draft')
+    return new Draft()
   }
 }
+
