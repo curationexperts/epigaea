972342450203fe9d351ee0a2ddf7078818408ee1
Tom Johnson <tom@dp.la>
2017-08-31 09:38:01 -0700

    Add shared examples for Batchable
    This improves the test coverage for `TemplateUpdate` and adds shared examples
    for Batchables in preparation for the introduction of new batch tasks.

--- spec/models/template_update_spec.rb
+++ spec/models/template_update_spec.rb
@@ -1,28 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe TemplateUpdate, type: :model do
-  subject(:update) { FactoryGirl.create(:template_update) }
+  subject(:batchable) { FactoryGirl.create(:template_update) }
+
+  it_behaves_like 'a batchable'
 
   describe '#behavior' do
     it 'is a writable attribute' do
-      expect { update.behavior = described_class::PRESERVE }
-        .to change { update.behavior }
+      expect { batchable.behavior = described_class::PRESERVE }
+        .to change { batchable.behavior }
         .to described_class::PRESERVE
     end
   end
 
   describe '#ids' do
     it 'is a writable attribute' do
-      expect { update.ids = ['moomin1', 'moomin2'] }
-        .to change { update.ids }
+      expect { batchable.ids = ['moomin1', 'moomin2'] }
+        .to change { batchable.ids }
         .to contain_exactly('moomin1', 'moomin2')
     end
   end
 
   describe '#template_name' do
     it 'is a writable attribute' do
-      expect { update.template_name = 'moomin' }
-        .to change { update.template_name }
+      expect { batchable.template_name = 'moomin' }
+        .to change { batchable.template_name }
         .to 'moomin'
     end
   end
