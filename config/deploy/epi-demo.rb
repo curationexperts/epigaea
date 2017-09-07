c922fd3246a849831947395b3501dab24f5b8ea1
Mark Bussey <mark@curationexperts.com>
2017-09-07 03:22:32 +0100

    Update dns for demo deployments

--- config/deploy/epi-demo.rb
+++ config/deploy/epi-demo.rb
@@ -1,2 +1,4 @@
 set :rails_env, 'production'
-server '52.21.87.2', user: 'deploy', roles: [:web, :app, :db]
+# server '52.21.87.2', user: 'deploy', roles: [:web, :app, :db]
+# server '52.72.119.114', user: 'deploy', roles: [:web, :app, :db]
+server 'epigaea.curationexperts.com', user: 'deploy', roles: [:web, :app, :db]
