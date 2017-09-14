1ea874e86bf7b250cb70230bbb19855a68f4dcae
Bess Sadler <bess@curationexperts.com>
2017-09-14 11:49:15 +0100

    mira_publication_workflow updates Loading mira workflow should be idempotent Change workflow role to publishing, and workflow action to publish Change workflow state of pending_review to unpublished Change workflow state of pending_review to unpublished Add unpublish workflow step Set mira publication workflow for default admin set in seeds.rb

--- spec/lib/tufts/workflow_setup_spec.rb
+++ spec/lib/tufts/workflow_setup_spec.rb
@@ -3,9 +3,33 @@ require 'tufts/workflow_setup'
 
 RSpec.describe Tufts::WorkflowSetup do
   let(:w) { described_class.new }
-  it "sets the default admin set to use the mira workflow" do
+  let(:admin_user) { FactoryGirl.create(:admin) }
+
+  it "sets the default admin set to use the mira workflow and runs idempotently" do
     w.set_default_workflow
-    a = ::AdminSet.find("admin_set/default")
-    expect(a.active_workflow.name).to eq "mira_publication_workflow"
+    a = AdminSet.find(AdminSet::DEFAULT_ID)
+    expect(a.active_workflow.name).to eq Tufts::WorkflowSetup::MIRA_WORKFLOW_NAME
+    w.set_default_workflow
+    b = AdminSet.find(AdminSet::DEFAULT_ID)
+    b.reload
+    # it has not created a new workflow with the same name
+    expect(b.active_workflow).to eq a.active_workflow
+  end
+  it "grants the workflow approval role to all members of the hyrax admin group" do
+    w.setup
+    a = AdminSet.find(AdminSet::DEFAULT_ID)
+    workflow = a.active_workflow
+    expect(a.active_workflow.name).to eq Tufts::WorkflowSetup::MIRA_WORKFLOW_NAME
+
+    # Check workflow permissions for admin user
+    roles = Hyrax::Workflow::PermissionQuery.scope_processing_workflow_roles_for_user_and_workflow(
+      user: admin_user,
+      workflow: workflow
+    )
+    role_names = roles.map { |r| Sipity::Role.find(r.role_id).name }
+
+    expect(role_names).to contain_exactly(
+      "publishing"
+    )
   end
 end
