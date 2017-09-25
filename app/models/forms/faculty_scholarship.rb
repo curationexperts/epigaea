cd2eae7f246947a10542e8467382bbc5fbe27f3a
Jamie Little <jamie@jamielittle.org>
2017-09-25 13:13:43 -0400

    Fix for submitting other authors on faculty scholarship form

--- app/models/forms/faculty_scholarship.rb
+++ app/models/forms/faculty_scholarship.rb
@@ -7,8 +7,7 @@ class FacultyScholarship < Contribution
 
     def copy_attributes
       super
-      @tufts_pdf.creator
-      @tufts_pdf.creator += [other_authors] if other_authors && other_authors != [""]
+      @tufts_pdf.creator << other_authors if other_authors && other_authors != [""]
     end
 
     def parent_collection
