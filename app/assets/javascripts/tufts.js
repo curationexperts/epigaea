e3f503bbadbc455baf42b1455c1c39c7826db570
Jamie Little <jamie@jamielittle.org>
2017-09-29 10:42:27 -0400

    This commits restores autocomplete for the Undergrad Thesis contribution form.
    This form was using Typeahead, but Hyrax has built in autocomplete functionality that
    works with local QA vocabs so that was used instead.
    
    There is a feature test searches for 'geol' and checks to see if the autocomplete returns 'Dept. of Geology'
    
    Hyrax doesn't include the autocomplete functionality in a global scope so I modified our global `Tufts` object
    to import it and make it available as `Tufts.autocomplete`. Additional fields will need to be added to that function
    if they need autocomplete.

--- app/assets/javascripts/tufts.js
+++ app/assets/javascripts/tufts.js
@@ -2,5 +2,11 @@ var Tufts = {
   qrStatus: function () {
     var QrStatus = require('tufts/qr_status')
     return new QrStatus()
+  },
+  autocomplete: function () {
+    var Autocomplete = require('hyrax/autocomplete')
+    var autocomplete = new Autocomplete()
+    
+    autocomplete.setup($('#contribution_department'),'default','/authorities/search/local/departments')
   }
 }
