31cfebde232339b05af60f1b2555814c50ddc894
Tom Johnson <tom@dp.la>
2017-10-10 15:07:24 -0700

    Add a real XML export to fixtures
    The export contains five objects generated by the `:populated_pdf` factory and
    exported with `Tufts::MetadataExporter`.

--- spec/factories/pdf.rb
+++ spec/factories/pdf.rb
@@ -6,4 +6,18 @@ FactoryGirl.define do
     title [FFaker::Book.title]
     visibility Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
   end
+
+  factory :populated_pdf, class: Pdf do
+    Pdf.properties.each_value do |property|
+      next if [:create_date, :modified_date, :has_model, :head, :tail].include? property.term
+
+      if property.term == :title
+        send(property.term, [FFaker::Book.title])
+      elsif property.try(:multiple?)
+        send(property.term, (1..4).map { FFaker::BaconIpsum.phrase })
+      else
+        send(property.term, FFaker::BaconIpsum.phrase)
+      end
+    end
+  end
 end
