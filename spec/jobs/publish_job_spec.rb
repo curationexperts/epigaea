62c472e4722144e0009ffc24dcdc9883c8fbfe59
Bess Sadler <bess@curationexperts.com>
2017-10-10 10:01:46 -0400

    publish_job moves a work to a published workflow state

---
+++ spec/jobs/publish_job_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe PublishJob, :workflow, type: :job do
+  subject(:job) { described_class }
+  let(:pdf) { FactoryGirl.create(:pdf) }
+
+  describe '#perform_later' do
+    it 'enqueues the job' do
+      ActiveJob::Base.queue_adapter = :test
+      expect { job.perform_later(pdf.id) }
+        .to enqueue_job(described_class)
+        .with(pdf.id)
+        .on_queue('batch')
+    end
+  end
+
+  describe '#workflow_user' do
+    it "finds or creates an admin user who can perform the workflow action" do
+      user = job.new.workflow_user
+      expect(user.user_key).to eq "batch_workflow_user@example.com"
+      expect(user.admin?).to eq true
+    end
+  end
+
+  context "workflow transition" do
+    let(:work) { FactoryGirl.create(:pdf) }
+    let(:depositing_user) { FactoryGirl.create(:user) }
+    before do
+      allow(CharacterizeJob).to receive(:perform_later) # Don't run fits
+      current_ability = ::Ability.new(depositing_user)
+      attributes = {}
+      env = Hyrax::Actors::Environment.new(work, current_ability, attributes)
+      Hyrax::CurationConcern.actor.create(env)
+    end
+    it 'sets the workflow status to published' do
+      expect(work.to_sipity_entity.reload.workflow_state_name).to eq "unpublished"
+      job.perform_now(work.id)
+      expect(work.to_sipity_entity.reload.workflow_state_name).to eq "published"
+    end
+  end
+end
