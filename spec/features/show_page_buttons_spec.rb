879ad404fbb26eb399e357ba2201eb5963ab7cf1
Jamie Little <jamie@jamielittle.org>
2017-10-10 14:29:48 -0400

    Adding a spec for testing buttons on the show page for works. The first sepc tests the mark as reviewed button.

---
+++ spec/features/show_page_buttons_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+# NOTE: If you generated more than one work, you have to set "js: true"
+RSpec.feature 'Create a PDF', :clean, js: true do
+  context 'a logged in admin user' do
+    let(:user) { FactoryGirl.create(:admin) }
+    let(:pdf) { FactoryGirl.create(:pdf) }
+
+    before { login_as user }
+
+    scenario do
+      visit "/concern/pdfs/#{pdf.id}"
+      # Specs for the buttons
+      find('.mark-as-reviewed').click
+      # The JS calls this as well, but Capybara doesn't seem to
+      # triggering Turbolinks in the same way
+      execute_script("Turbolinks.visit(window.location.href)")
+      expect(page).to have_css('.mark-as-reviewed[disabled]')
+    end
+  end
+end
