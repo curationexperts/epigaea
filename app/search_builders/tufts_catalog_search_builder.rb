8651cae1f90452bde982b8ef73784b236abec787
Bess Sadler <bess@curationexperts.com>
2017-10-19 15:53:56 -0400

    Show all works, regardless of publication status, to admins in search results

---
+++ app/search_builders/tufts_catalog_search_builder.rb
@@ -0,0 +1,7 @@
+class TuftsCatalogSearchBuilder < Hyrax::CatalogSearchBuilder
+  def show_only_active_records(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << '-suppressed_bsi:true' unless current_user.admin?
+    solr_parameters[:fq].delete("-suppressed_bsi:true") if current_user.admin?
+  end
+end
