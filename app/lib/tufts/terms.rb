05936841eb159cb50ad2a926b6780974317a06c3
Jamie Little <jamie@jamielittle.org>
2017-10-20 15:08:18 -0400

    Resource Type was duplicated in  and in the Tufts attributes. This removes the custom Tufts one and uses the Hyrax version because they share the same predicate

--- app/lib/tufts/terms.rb
+++ app/lib/tufts/terms.rb
@@ -4,7 +4,7 @@ module Tufts
                     :abstract, :table_of_contents, :primary_date, :date_accepted,
                     :date_available, :date_copyrighted, :date_issued, :steward,
                     :internal_note, :audience, :embargo_note, :end_date, :accrual_policy,
-                    :rights_note, :resource_type,
+                    :rights_note,
                     :bibliographic_citation, :rights_holder, :format_label, :replaces, :is_replaced_by,
                     :has_format, :is_format_of, :has_part, :tufts_license,
                     :retention_period, :admin_start_date, :qr_status, :rejection_reason,
