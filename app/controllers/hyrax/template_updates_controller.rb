3d66b11d029d3e511acc99b34713240d1612a1d3
Jamie Little <jamie@jamielittle.org>
2017-10-20 12:57:00 -0400

    Redirect back to template selection if you pick the placeholder in the select box

--- app/controllers/hyrax/template_updates_controller.rb
+++ app/controllers/hyrax/template_updates_controller.rb
@@ -1,6 +1,11 @@
 module Hyrax
   class TemplateUpdatesController < ApplicationController
     def create
+      if params[:template_update][:template_name] == ""
+        redirect_to main_app.new_template_update_path(ids: template_update_params[:ids])
+        flash[:warning] = "Please select a valid template"
+        return
+      end
       update       = TemplateUpdate.new(template_update_params)
       update.batch = Batch.create(batchable: update,
                                   creator:   current_user,
