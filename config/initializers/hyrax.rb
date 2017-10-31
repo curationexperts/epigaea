1af0d54492c6d863936515fa7b8d740959893292
Jamie Little <jamie@jamielittle.org>
2017-10-31 10:14:33 -0400

    Set the default Hyrax workflow to the MIRA workflow

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -35,7 +35,7 @@ Hyrax.config do |config|
   # When an admin set is created, we need to activate a workflow.
   # The :default_active_workflow_name is the name of the workflow we will activate.
   # @see Hyrax::Configuration for additional details and defaults.
-  # config.default_active_workflow_name = 'default'
+  config.default_active_workflow_name = 'mira_publication_workflow'
 
   # Email recipient of messages sent via the contact form
   # config.contact_email = "repo-admin@example.org"
