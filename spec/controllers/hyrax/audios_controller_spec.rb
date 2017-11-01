542e0b5ed0a7515f2d5b0413f7a2aaaf2c5b6240
Bess Sadler <bess@curationexperts.com>
2017-11-01 17:21:23 -0400

    Normalize whitespace for all works

--- spec/controllers/hyrax/audios_controller_spec.rb
+++ spec/controllers/hyrax/audios_controller_spec.rb
@@ -3,7 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::AudiosController do
-  it "has tests" do
-    skip "Add your tests here"
+  describe 'normalize spaces before passing them to object creation methods' do
+    let(:params) do
+      {
+        "utf8" => "✓",
+        "audio" =>
+        {
+          "title" => " Space   non normalized  \t title    ",
+          "creator" => " Name   with  Spaces ",
+          "bibliographic_citation" => [" bibliographic   citation   with     spaces    ", ""],
+          "rights_holder" => ["blah     spaces   ", ""],
+          "publisher" => ["Publisher  ", ""],
+          "description" => [" A short   description \n   with  wonky spaces.\r\n\r\n\r\nBut keep two newlines between paragraphs. ", ""],
+          "abstract" => [" A short   description \n   with  wonky spaces.\r\n\r\n\r\nBut keep two newlines between paragraphs. ", ""]
+        },
+        "controller" => "hyrax/audios",
+        "action" => "update"
+      }
+    end
+    it "normalizes the text in the params hash" do
+      described_class.new.normalize_whitespace(params)
+      expect(params["audio"]["title"]).to eq "Space non normalized title"
+      expect(params["audio"]["creator"]).to eq "Name with Spaces"
+      expect(params["audio"]["bibliographic_citation"]).to contain_exactly("bibliographic citation with spaces", "")
+      expect(params["audio"]["rights_holder"]).to contain_exactly("blah spaces", "")
+      expect(params["audio"]["description"]).to contain_exactly("A short description \n with wonky spaces.\n\nBut keep two newlines between paragraphs.", "")
+      expect(params["audio"]["abstract"]).to contain_exactly("A short description \n with wonky spaces.\n\nBut keep two newlines between paragraphs.", "")
+    end
   end
 end
