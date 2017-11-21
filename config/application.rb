4d341946ae73a1391afcf33687eeac1fe5d5646f
Bess Sadler <bess@curationexperts.com>
2017-11-21 18:19:53 -0500

    change HYRAX_HOST to RAILS_HOST

--- config/application.rb
+++ config/application.rb
@@ -30,4 +30,4 @@ module Epigaea
     end
   end
 end
-Rails.application.routes.default_url_options[:host] = ENV["HYRAX_HOST"]
+Rails.application.routes.default_url_options[:host] = ENV["RAILS_HOST"]
