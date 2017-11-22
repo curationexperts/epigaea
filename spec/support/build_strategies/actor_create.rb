371d612af721467832a7e28ad543a417f6dbfe21
Tom Johnson <tom@dp.la>
2017-11-22 11:58:40 -0800

    Don't mint handles unless minimum requirements are met
    We only want to mint and update handles for items that are in 'dl' and are published.

--- spec/support/build_strategies/actor_create.rb
+++ spec/support/build_strategies/actor_create.rb
@@ -12,7 +12,7 @@ class ActorCreate
       evaluator = evaluation.instance_variable_get(:@attribute_assigner)
                             .instance_variable_get(:@evaluator)
 
-      ability = Ability.new(evaluator.user)
+      ability = Ability.new(user(evaluator))
       env     = Hyrax::Actors::Environment.new(instance, ability, {})
 
       evaluation.notify(:before_create,       instance)
@@ -22,4 +22,17 @@ class ActorCreate
       evaluation.notify(:after_actor_create, instance)
     end
   end
+
+  private
+
+    def user(evaluator)
+      user = evaluator.user
+
+      if user.nil? || user.new_record?
+        raise 'You must pass a created depositing user to use the `actor_create` ' /
+              "build strategy; received: #{user}"
+      end
+
+      user
+    end
 end
