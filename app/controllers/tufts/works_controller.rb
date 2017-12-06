2f8f58f2cf8235e5b10061ef851ff3035f900606
Jamie Little <jamie@jamielittle.org>
2017-12-06 14:13:32 -0500

    Change the partial for rights statement to submit an array for the rights_statement attribute, not a string. Remove the previous solution which altered the value in the works_controller

--- app/controllers/tufts/works_controller.rb
+++ app/controllers/tufts/works_controller.rb
@@ -6,13 +6,11 @@ module Tufts
     include Tufts::Normalizer
     def create
       normalize_whitespace(params)
-      rights_statement_scalar_fix(params)
       super
     end
 
     def update
       normalize_whitespace(params)
-      rights_statement_scalar_fix(params)
       delete_draft(params)
       super
     end
@@ -22,16 +20,6 @@ module Tufts
       def delete_draft(params)
         work = ActiveFedora::Base.find(params['id'])
         work.delete_draft
-      end
-
-      def rights_statement_scalar_fix(params)
-        index = if Rails.env.production?
-                  2
-                else
-                  3
-                end
-        return if params[params.keys[index]]['rights_statement'].nil?
-        params[params.keys[index]]['rights_statement'] = [params[params.keys[index]]['rights_statement']]
       end
   end
 end
