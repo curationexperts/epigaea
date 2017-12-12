63992eefdb99ab2f390cc5dcbd9e16a2545609e3
Bess Sadler <bess@curationexperts.com>
2017-12-12 12:42:22 -0500

    Check for required fields
    1. Move the XML validation check to the validate! method
    
    2. When there are XML validation errors only report the
    first one. A single stray element causes six errors because
    it cascades -- really, the user just needs to know the
    document is malformed and they should go check it with
    an XML parser.
    
    3. Add a method to validate! that checks for required fields.
    Right now this includes "dc:title", "tufts:displays_in",
    and "model:hasModel". Eventually we might want to put this in
    a config somewhere.
    
    4. Explicitly add namespaces of the required fields we're
    checking for to the in-memory XML document. The way we
    declare XML namespaces varies across some of the test files
    we're using, and this ensures the validation works across
    that variation.
    
    5. Add required displays_in element to sample import file

--- spec/features/xml_import_spec.rb
+++ spec/features/xml_import_spec.rb
@@ -49,7 +49,18 @@ RSpec.feature 'Create an XML Import', :clean, js: true do
     attach_file 'metadata_file', File.join(fixture_path, 'files', 'malformed_files', 'stray_element.xml')
 
     click_button 'Next'
-
     expect(page).to have_content 'Error'
+    expect(page).to have_content 'Malformed XML'
+  end
+
+  scenario 'importing a file with missing required fields' do
+    visit '/xml_imports/new'
+
+    attach_file 'metadata_file', File.join(fixture_path, 'files', 'malformed_files', 'missing_required_fields.xml')
+
+    click_button 'Next'
+    expect(page).to have_content 'Missing required field: Sonnet_1.pdf is missing tufts:displays_in'
+    expect(page).to have_content 'Missing required field: Sonnet_1.pdf is missing dc:title'
+    expect(page).to have_content 'Missing required field: Sonnet_1.pdf is missing model:hasModel'
   end
 end
