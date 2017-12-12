c92795b5a0ef1be6429a285f11d5369d79152980
Jamie Little <jamie@jamielittle.org>
2017-12-12 13:41:04 -0500

    Remove access to admin areas to non-admin users
    This is a refactor of `routes.rb` to enforce access rules
    for non-admin users.
    
    Since non-admin users can notifications on the dashboard, this
    updates the specs for the sidebar links to ensure that
    non-admin users don't see links to admin functionality in the
    dashboard.
    
    This commit also ensures that there is a flash message when
    users are redirected from paths that they don't have access
    to or try access a non-existent path.
    
    Closes #733

--- config/routes.rb
+++ config/routes.rb
@@ -7,48 +7,95 @@ Rails.application.routes.draw do
     request.env['warden'].authenticate? && !request.env['warden'].user.admin?
   end
 
-  mount Blacklight::Engine => '/'
-  mount BlacklightAdvancedSearch::Engine => '/'
-
-  # Mount sidekiq web ui and require authentication by an admin user
-  require 'sidekiq/web'
-  authenticate :user, ->(u) { u.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
-
-  concern :searchable, Blacklight::Routes::Searchable.new
-
   constraints admin_constraint do
     root to: 'hyrax/dashboard#show'
+
+    concern :exportable, Blacklight::Routes::Exportable.new
+    concern :searchable, Blacklight::Routes::Searchable.new
+
     # Only admin users should be able to search
     resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
       concerns :searchable
     end
-  end
+    mount BlacklightAdvancedSearch::Engine => '/'
 
-  devise_for :users
+    # Mount sidekiq web ui and require authentication by an admin user
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+
+    resources :batches, controller: 'hyrax/batches', only: [:index, :show, :create]
+
+    resources :metadata_exports,
+              controller: 'hyrax/metadata_exports',
+              only:       [:create]
+    resources :metadata_imports,
+              controller: 'hyrax/metadata_imports',
+              only:       [:new, :create]
+    resources :xml_imports,
+              controller: 'hyrax/xml_imports',
+              only:       [:show, :create, :new, :edit, :update]
+    resources :templates,
+              controller: 'hyrax/templates',
+              only:       [:index, :destroy, :edit, :update, :new]
+    resources :template_updates,
+              controller: 'hyrax/template_updates',
+              only:       [:index, :new, :create]
+
+    get '/metadata_exports/:id/download', to: 'hyrax/metadata_exports#download'
+
+    # Routes for managing drafts
+    post '/draft/save_draft/:id', to: 'tufts/draft#save_draft'
+    post '/draft/delete_draft/:id', to: 'tufts/draft#delete_draft'
+    get '/draft/draft_saved/:id', to: 'tufts/draft#draft_saved'
+
+    # Routes for managing QR status
+    resources :qr_statuses, controller: 'tufts/qr_status', only: [:set_status, :status] do
+      member do
+        post 'set_status'
+        get 'status'
+      end
+    end
+
+    # Routes for managing publication status
+    resources :publication_statuses, controller: 'tufts/publication_status', only: [:publish, :unpublish, :status] do
+      member do
+        post 'publish'
+        post 'unpublish'
+        get 'status'
+      end
+    end
+
+    resources :deposit_types do
+      get 'export', on: :collection
+    end
+
+    # Routes for managing QR status
+
+    get '/handle/log/', to: 'tufts/handle_log#index'
+  end # end admin constraint
 
   constraints non_admin_constraint do
     root to: 'contribute#redirect'
     get '/dashboard', to: 'contribute#redirect'
   end
 
-  mount Hydra::RoleManagement::Engine => '/'
+  devise_for :users
 
+  # Mount Engines
+  mount Blacklight::Engine => '/'
+  mount Hydra::RoleManagement::Engine => '/'
   mount Qa::Engine => '/authorities'
   mount Hyrax::Engine, at: '/'
+
+  # Home page for non-authenticated users
   resources :welcome, only: 'index'
   root 'hyrax/homepage#index'
 
   curation_concerns_basic_routes
 
-  concern :exportable, Blacklight::Routes::Exportable.new
-
   resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
     concerns :exportable
   end
-
-  resources :batches, controller: 'hyrax/batches', only: [:index, :show, :create]
 
   resources :bookmarks do
     concerns :exportable
@@ -58,61 +105,12 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :metadata_exports,
-            controller: 'hyrax/metadata_exports',
-            only:       [:create]
-  resources :metadata_imports,
-            controller: 'hyrax/metadata_imports',
-            only:       [:new, :create]
-  resources :xml_imports,
-            controller: 'hyrax/xml_imports',
-            only:       [:show, :create, :new, :edit, :update]
-  resources :templates,
-            controller: 'hyrax/templates',
-            only:       [:index, :destroy, :edit, :update, :new]
-  resources :template_updates,
-            controller: 'hyrax/template_updates',
-            only:       [:index, :new, :create]
-
-  get '/metadata_exports/:id/download', to: 'hyrax/metadata_exports#download'
-
-  # Routes for managing drafts
-  post '/draft/save_draft/:id', to: 'tufts/draft#save_draft'
-  post '/draft/delete_draft/:id', to: 'tufts/draft#delete_draft'
-  get '/draft/draft_saved/:id', to: 'tufts/draft#draft_saved'
-
-  # Routes for managing QR status
-  resources :qr_statuses, controller: 'tufts/qr_status', only: [:set_status, :status] do
-    member do
-      post 'set_status'
-      get 'status'
-    end
-  end
-
-  # Routes for managing publication status
-  resources :publication_statuses, controller: 'tufts/publication_status', only: [:publish, :unpublish, :status] do
-    member do
-      post 'publish'
-      post 'unpublish'
-      get 'status'
-    end
-  end
-
-  resources :deposit_types do
-    get 'export', on: :collection
-  end
-
   resources :contribute, as: 'contributions', controller: :contribute, only: [:index, :new, :create, :redirect] do
     collection do
       get 'license'
     end
   end
 
-  # Routes for managing QR status
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/handle/log/', to: 'tufts/handle_log#index'
-
   # needs to be last to redirect /bad/paths
-  get '*path', to: 'contribute#redirect'
+  get '*path' => redirect { |_, req| req.flash[:error] = "You are not authorized to access this page"; 'contribute#redirect' } # rubocop:disable  Style/Semicolon
 end
