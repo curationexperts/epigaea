bd1f9dd8b5fe147cfa7fb657092d327aea8342eb
Jamie Little <jamie@jamielittle.org>
2017-12-13 12:12:29 -0500

    Give admin users access to all works in Collections views
    This overrides the `only_active_works` method in
    `CollectionMemberSearchBuilder` so that admin users see
    unpulished works in the collections views.
    
    Closes #685

--- app/search_builders/hyrax/collection_member_search_builder.rb
+++ app/search_builders/hyrax/collection_member_search_builder.rb
@@ -10,10 +10,21 @@ module Hyrax
 
     delegate :collection, to: :scope
 
+    def only_active_works(solr_parameters)
+      return if current_user.admin?
+      super
+    end
+
     # include filters into the query to only include the collection memebers
     def member_of_collection(solr_parameters)
       solr_parameters[:fq] ||= []
-      solr_parameters[:fq] << "#{collection_membership_field}:#{collection.id}"
+      solr_parameters[:fq] << "#{collection_membership_field}:#{collection_id}"
     end
+
+    private
+
+      def collection_id
+        collection.id || raise("Collection does not have an identifier")
+      end
   end
 end
